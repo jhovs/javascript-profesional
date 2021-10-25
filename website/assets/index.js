eb6c2850553bed775be2b386959d5a61aadaabc8
jhovan <jhovan.solano@gmail.com>
2021-10-25 12:55:58 -0500

    Modulo 06 - Clase 21: IntersectionObserver | Finalizado

--- website/assets/index.js
+++ website/assets/index.js
@@ -1,11 +1,13 @@
 import MediaPlayer from './mediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPuse from './plugins/AutoPause.js';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
     el: video, 
     plugins: [
-    //    new AutoPlay()
+     new AutoPlay(),
+     new AutoPuse()
     ]});
 
 const button = document.querySelector('#buttonPlay');
